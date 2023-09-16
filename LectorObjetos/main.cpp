0651bb6fef447b096a19848146cd9fe0dfdcffbb
SantiagoEsc1216 <santiagoesc1216@gmail.com>
2023-09-16 11:31:01 -0600

    opengl

--- LectorObjetos/main.cpp
+++ LectorObjetos/main.cpp
@@ -1,15 +1,67 @@
-#include "Lector.h"
+#include <GL/freeglut.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
-int main() {
+float grados = 0;
+void display(void)
+{
+	/*  clear all pixels  */
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	Lector lector;
-	vector<Modelo> modelos;
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_TRIANGLES);
+	glVertex3f(0.25, 0.75, 0.0);
+	glVertex3f(0.5, 0.75, 0.0);
+	glVertex3f(0.5, 0.9, 0.0);
+	glEnd();
 
-	modelos = lector.leerArchivo("prueba.obj");
-	
-	for (Modelo modelo : modelos) {
-		modelo.imprimirModelo();
-	}
+	glutSwapBuffers();
+	glFlush();
+}
 
-	return 0;
+void init(void)
+{
+	/*  select clearing (background) color       */
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
+	/*  initialize viewing values  */
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	//glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+	//gluOrtho2D(0.0, 1.0, 0.0, 1.0);
+
+	gluPerspective(45.0, 800.0 / 600.0, 0.1, 100.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(0.0, 5.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+	glEnable(GL_DEPTH_TEST);
+
+}
+
+/*
+ *  Declare initial window size, position, and display mode
+ *  (single buffer and RGBA).  Open window with "hello"
+ *  in its title bar.  Call initialization routines.
+ *  Register callback function to display graphics.
+ *  Enter main loop and process events.
+ */
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("hello");
+	init();
+	glutDisplayFunc(display);
+	//glutPostRedisplay();
+	glutIdleFunc(display);
+	glutMainLoop();
+
+	return 0;   /* ISO C requires main to return int. */
 }
